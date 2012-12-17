4a923c019aafcdc1f99e61806410315048ad27f0
Andrés Caro <andres.caro.q@gmail.com>
2012-12-17 09:57:26 +0000

    Fixing forms style

--- src/MenUSACH/Bundle/BaseBundle/Form/LocalType.php
+++ src/MenUSACH/Bundle/BaseBundle/Form/LocalType.php
@@ -11,16 +11,32 @@ class LocalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('loc_nombre')
-            ->add('loc_ubicacion')
+            ->add('loc_nombre', 'text', array(
+                'label' => 'Nombre de local',
+                'attr' => array(
+                    'class' => 'span3',
+                    'placeholder' => 'Nombre'
+            )))
+            ->add('loc_ubicacion', 'text', array(
+                'label' => 'Ubicación del local',
+                'attr' => array(
+                    'class' => 'span3',
+                    'placeholder' => 'Ubicación'
+            )))
             ->add('propietario',
-                  'entity', array('class' => 'MenUSACH\Bundle\BaseBundle\Entity\Propietario',
-                  'property' => 'nombreCompleto'))
+                  'entity', array(
+                      'class' => 'MenUSACH\Bundle\BaseBundle\Entity\Propietario',
+                      'property' => 'nombreCompleto',
+                      'attr' => array(
+                          'class' => 'span3'
+            )))
             ->add('tiposdepago',
-                  'entity', array('class' => 'MenUSACH\Bundle\BaseBundle\Entity\TipoPago',
-                  'property' => 'tp_descripcion',
-                  'expanded' => true ,
-                  'multiple' => true ));
+                  'entity', array(
+                      'class' => 'MenUSACH\Bundle\BaseBundle\Entity\TipoPago',
+                      'property' => 'tp_descripcion',
+                      'expanded' => true ,
+                      'multiple' => true
+            ));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
